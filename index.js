dc295aed51fbf3de11577a54dc101a08e4921b24
Asadiftekhar <143116178+asad755@users.noreply.github.com>
2024-04-05 17:17:03 +0500

    gone

---
+++ index.js
@@ -0,0 +1,21 @@
+import inquirer from "inquirer";
+let todos = [];
+let condition = true;
+while (condition) {
+    let todoquestions = await inquirer.prompt([
+        {
+            name: "firstQuestion",
+            type: "input",
+            message: "what would you like to add in  your todo?"
+        },
+        {
+            name: "secondQuestion",
+            type: "confirm",
+            message: "would you like to add another todo?",
+            default: "true"
+        }
+    ]);
+    todos.push(todoquestions.firstQuestion);
+    console.log(todos);
+    condition = todoquestions.secondQuestion;
+}
